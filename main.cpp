17e4e58401cc1789e55667a7c0d00883dd50fc07
utopian01 <monicabhalshankar@gmail.com>
2020-07-12 16:17:22 +0000

    added code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  
+  vector<int> A{9,9,9};
+  int i = A.size() - 1;
+  int carry = 1;
+  int tmp;
+  while(i>=0){
+    tmp = A[i] + carry;
+    if(tmp == 10){
+      A[i] = 0;
+      carry =1;
+    }
+    else{
+      A[i] = tmp;
+      carry = 0;
+    }
+    i--;
+  }
+  if(carry)
+    A.insert(A.begin(),carry);
+  std::cout << "Incremented value: " ;
+  for(int i=0; i < A.size(); i++)
+   std::cout << A.at(i) << ' ';
 }
